3227146a8fb60ed6a5273c9a8632ac1b8843beda
Sollute <sollute@senemby.local>
2020-10-27 11:18:25 -0300

    ac3

---
+++ t8.py
@@ -0,0 +1,15 @@
+import os
+from flask import flask, jsonify, request
+from math import sqrt
+
+app = flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+
+    primos = "Tudo vai dar certo caros alunos!"
+
+    return primos
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
